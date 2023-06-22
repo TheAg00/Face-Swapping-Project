c80f086a068884565145ba26c281b84191ec5f10
TheAg00 <a.gkogkos.ag@gmail.com>
2023-06-22 03:17:05 +0300

    Finished project

--- project.py
+++ project.py
@@ -2,15 +2,21 @@ import cv2
 
 if __name__ == "__main__":
     # Φορτώνουμε την εικόνα.
-    image = cv2.imread('image.jpg')
+    originalImage = cv2.imread("image.jpg")
+
+    if originalImage is None:
+        print("Failed to load the image!")
+        exit()
+
+    swapedImage = cv2.imread('image.jpg')
 
     # Ελέγχουμε αν η εικόνα φορτώθηκε επιτυχώς
-    if image is None:
+    if swapedImage is None:
         print("Failed to load the image!")
         exit()
 
     # Μετατρέπουμε την εικόνα σε grey scaled.
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(swapedImage, cv2.COLOR_BGR2GRAY)
 
     # Φορτώνουμε το pre-trained classifier για την αναγνώριση προσώπου.
     haarCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -20,7 +26,7 @@ if __name__ == "__main__":
 
     # Προσδιορίζουμε την περιοχή του προσώπου.
     for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), thickness = 2)
+        cv2.rectangle(swapedImage, (x, y), (x + w, y + h), (0, 255, 0), thickness = 2)
 
     # Αν δεν υπάρχουν ακριβώς 2 πρόσωπα στην εικόνα, εμφανίζει κατάλληλο μήνυμα και τερματίζει το πρόγραμμα.
     if len(faces) != 2:
@@ -31,22 +37,24 @@ if __name__ == "__main__":
     face1 = faces[0]
     face2 = faces[1]
 
-    # Παίρνουμε τα περιγράμματα των 2 προσώπων.
+    # Παίρνουμε τα 2 πρόσωπα.
     x1, y1, w1, h1 = face1
     x2, y2, w2, h2 = face2
-    faceRegion1 = image[y1:y1+h1, x1:x1+w1]
-    faceRegion2 = image[y2:y2+h2, x2:x2+w2]
+    faceRegion1 = swapedImage[y1:y1+h1, x1:x1+w1]
+    faceRegion2 = swapedImage[y2:y2+h2, x2:x2+w2]
 
     # Προσαρμόζουμε το μέγεθος των περιγραμμάτων των προσπώπων για να ταιριάζουν.
     faceRegion1 = cv2.resize(faceRegion1, (w2, h2))
     faceRegion2 = cv2.resize(faceRegion2, (w1, h1))
 
     # Αντικαθιστούμε τα πρόσωπα.
-    image[y1:y1+h1, x1:x1+w1] = faceRegion2
-    image[y2:y2+h2, x2:x2+w2] = faceRegion1
+    swapedImage[y1:y1+h1, x1:x1+w1] = faceRegion2
+    swapedImage[y2:y2+h2, x2:x2+w2] = faceRegion1
 
     # Εμφανίζουμε το αποτέλεσμα.
     cv2.namedWindow('Face Detection', cv2.WINDOW_NORMAL)
-    cv2.imshow('Face Detection', image)
+    cv2.namedWindow('Face Swaped', cv2.WINDOW_NORMAL)
+    cv2.imshow('Face Detection', originalImage)
+    cv2.imshow('Face Swaped', swapedImage)
 
     cv2.waitKey(0)
